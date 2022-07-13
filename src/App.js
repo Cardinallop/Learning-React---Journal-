fc5e40c08ccd56fd5cd396bf3583c404b641c457
Bobur Murtozaev <boburmurtozaev00@gmail.com>
2022-07-13 18:26:47 -0500

    //yes

--- src/App.js
+++ src/App.js
@@ -4,16 +4,17 @@ import './App.css';
 import ErrorBoundary from './components/ErrorBoundary';
 import Header from './components/Header';
 import Router from './components/Router';
+import {User } from './components/UserContext';
 
 function App() {
   return (
     <ErrorBoundary>
       <BrowserRouter>
-        <div>
+        <div >
 
           <Header />
           <Router />
-
+          <User/>
         </div>
       </BrowserRouter>
     </ErrorBoundary>
